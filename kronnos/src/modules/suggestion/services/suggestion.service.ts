842a82987139d05a570d5f533c6a1e51fdb5b22c
Mikaela Maria Cardenas Rodriguez <mikamoontaurus@gmail.com>
2022-03-07 17:09:39 -0400

    Suggestions module

--- kronnos/src/modules/suggestion/services/suggestion.service.ts
+++ kronnos/src/modules/suggestion/services/suggestion.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Suggestion } from 'src/model/Suggestion';
+import { User } from 'src/model/User';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
-export class SuggestionService {}
+export class SuggestionService {
+
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository:Repository<User>,
+
+        @InjectRepository(Suggestion)
+        private readonly suggestionRepository:Repository<Suggestion>
+    ){}
+
+    async saveSuggestion(suggestion: Suggestion){
+        return await this.suggestionRepository.save(suggestion);
+    }
+
+    async showAllSuggestions(){
+        return await this.suggestionRepository.find();
+    }
+
+    async findOneSuggestion(id:number):Promise<Suggestion>{
+        return await this.suggestionRepository.findOne({});
+    }
+
+    async deleteSuggestion(id:number):Promise<DeleteResult>{
+        return await this.suggestionRepository.delete({idSuggestion:id});
+    }
+    
+
+}
--- kronnos/src/modules/suggestion/suggestion.module.ts
+++ kronnos/src/modules/suggestion/suggestion.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Suggestion } from 'src/model/Suggestion';
+import { User } from 'src/model/User';
 import { SuggestionController } from './controllers/suggestion.controller';
 import { SuggestionService } from './services/suggestion.service';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([
+    User,
+    Suggestion
+  ])],
   controllers: [SuggestionController],
   providers: [SuggestionService]
 })
