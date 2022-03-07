30ad3b0f44564f096506d3871dc13f9c52b6acab
AleS900 <santiavctf@gmail.com>
2022-03-07 17:06:33 -0400

    News Module

--- kronnos/src/app.module.ts
+++ kronnos/src/app.module.ts
@@ -3,23 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
+import { NewsModule } from './modules/news/news.module';
+
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot({
-        name: 'default',
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'root',
-        database: 'kronnosDB',
-        schema:'public',
-        synchronize: true,
-        entities:[__dirname + '/model/**/*{.ts,.js}'],
-        autoLoadEntities: true,
-      }),
-    UserModule],
+    TypeOrmModule.forRoot({
+      name: 'default',
+      type: 'postgres',
+      host: 'localhost',//'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '11037',
+      database: 'login',
+      schema: 'public',
+      synchronize: true,
+      entities: [__dirname + '/model/**/*{.ts,.js}'],
+      autoLoadEntities: true,
+    }),
+    UserModule,
+    NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
